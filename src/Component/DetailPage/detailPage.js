9550dae454acd1fa76a2b839c5a68c4453f9d0c4
MuskanSrivastava8 <muskansrivastava8756@gmail.com>
2024-05-25 18:38:43 +0530

    Detail Page design improved now

--- src/Component/DetailPage/detailPage.js
+++ src/Component/DetailPage/detailPage.js
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom";
 import "./detailPage.scss";
 import { useSelector } from "react-redux";
 import Rating from "@mui/material/Rating";
+import { CiDeliveryTruck } from "react-icons/ci";
 
 const DetailPage = (catagoryName) => {
   const [showCatagoryPage, setshowCatagoryPage] = useState(false);
@@ -17,13 +18,13 @@ const DetailPage = (catagoryName) => {
     return i.id === itemSelectedResp;
   });
   let priceBeforeDiscount;
-  if(itemSelectedResult){
+  if (itemSelectedResult) {
     priceBeforeDiscount = (
-    ((itemSelectedResult[0].discountPercentage + 100) *
-      itemSelectedResult[0].price) /
-    100
-  ).toFixed(2);
-}
+      ((itemSelectedResult[0].discountPercentage + 100) *
+        itemSelectedResult[0].price) /
+      100
+    ).toFixed(2);
+  }
   const [value, setValue] = React.useState(itemSelectedResult[0].rating);
 
   console.log(itemSelectedResult && itemSelectedResult[0]);
@@ -45,86 +46,146 @@ const DetailPage = (catagoryName) => {
               <IoIosArrowRoundBack style={{ fontSize: "2rem" }} />
             </Button>
           </div>
-          <div className="detailPage_data_main">
-            {/* <div className="detailPage_Address">
-            <div>Adress</div>
-            <div>Edit Address</div>
-          </div> */}
-            <div className="detailPage_image_data">
-              <div>
-                <img
-                  src={itemSelectedResult[0].images[0]}
-                  alt="productImage"
-                  style={{ height: "100%", width: "100%" }}
-                />
-              </div>
-              <div className="details_data">
-                <div style={{ fontWeight: "700", fontSize: "1rem" }}>
-                  {itemSelectedResult[0].title}
-                </div>
-                <div style={{ fontSize: "small", color: "gray" }}>
-                  {itemSelectedResult[0].brand}
-                </div>
-                <div style={{ fontSize: "1rem" }}>
-                  In {itemSelectedResult[0].category}
-                </div>
-                <div style={{ fontSize: "1rem" }}>
-                  {itemSelectedResult[0].description}
-                </div>
-                <div style={{ fontSize: "1rem" }}>
-                  Hurry Up only {itemSelectedResult[0].stock} left
-                </div>
-                <div
-                  style={{
-                    color: "green",
-                    fontWeight: "700",
-                    fontSize: ".8rem",
-                  }}
-                >
-                  {" "}
-                  Special price
-                </div>
-                <div style={{ display: "flex", gap: ".5rem" }}>
-                  <div style={{ fontWeight: "700", fontSize: ".8rem" }}>
-                    Rs {itemSelectedResult[0].price}
+          <div className="detailPage_main_div">
+            <div className="detailPage_image_main">
+              <img
+                src={itemSelectedResult[0].images[0]}
+                alt="productImage"
+                style={{ height: "100%", width: "100%" }}
+              />
+            </div>
+            <div className="detailPage_data_main">
+              <div className="detailPage_image_data">
+                <div className="details_data">
+                  <div style={{ fontWeight: "700", fontSize: "1rem" }}>
+                    {itemSelectedResult[0].title}
+                  </div>
+                  <div style={{ fontSize: "small", color: "gray" }}>
+                    {itemSelectedResult[0].brand}
+                  </div>
+                  <div style={{ fontSize: "1rem",textTransform: "capitalize" }}>
+                    <span
+                      style={{
+                        fontWeight: "700",
+                        fontSize: "0.8rem",
+                        color:"#878787",
+                      }}
+                    >
+                      Catagory 
+                    </span>{" "}
+                    {itemSelectedResult[0].category}
+                  </div>
+                  <div style={{ fontSize: "1rem" }}>
+                    {itemSelectedResult[0].description}
                   </div>
                   <div
                     style={{
-                      fontWeight: "200",
-                      fontSize: "small",
-                      color: "gray",
-                      textDecoration: "line-through",
-                    }}
-                  >
-                    ({priceBeforeDiscount})
-                  </div>
-                  <div
-                    style={{
-                      fontWeight: "100",
-                      fontSize: ".7rem",
                       color: "red",
+                      fontWeight: "500",
+                      fontSize: "0.8rem",
                     }}
                   >
-                    ({itemSelectedResult[0].discountPercentage} % OFF)
+                    Hurry Up only {itemSelectedResult[0].stock} left
                   </div>
-                </div>
-                <div>
-                  <Rating name="read-only" value={value} readOnly />
-                </div>
-                <div className="detail_extra_info">
-                  <div>100% Original Products</div>
-                  <div>Pay on delivery might be available</div>
-                  <div>Easy 14 days returns and exchanges</div>
+                  <div
+                    style={{
+                      color: "green",
+                      fontWeight: "700",
+                      fontSize: ".8rem",
+                    }}
+                  >
+                    Special price
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <div style={{ fontWeight: "700", fontSize: ".8rem" }}>
+                      Rs {itemSelectedResult[0].price}
+                    </div>
+                    <div
+                      style={{
+                        fontWeight: "200",
+                        fontSize: "small",
+                        color: "gray",
+                        textDecoration: "line-through",
+                      }}
+                    >
+                      ({priceBeforeDiscount})
+                    </div>
+                    <div
+                      style={{
+                        fontWeight: "100",
+                        fontSize: ".7rem",
+                        color: "red",
+                      }}
+                    >
+                      ({itemSelectedResult[0].discountPercentage} % OFF)
+                    </div>
+                  </div>
+                  <div>
+                    <Rating name="read-only" value={value} readOnly />
+                  </div>
+                  <div
+                    style={{
+                      fontWeight: "500",
+                      color: "#878787",
+                      fontSize: "0.8rem",
+                    }}
+                  >
+                    {Math.trunc(Math.random() * 500)} Ratings &{" "}
+                    {Math.trunc(Math.random() * 50)} Reviews
+                  </div>
+                  <div className="detail_extra_info">
+                    <div>100% Original Products</div>
+                    <div>Pay on delivery might be available</div>
+                    <div>Easy 14 days returns and exchanges</div>
+                  </div>
+                  <div className="detailPage_btn_set">
+                    <div>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        onClick={navigateToCatagoryPage}
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        onClick={navigateToCatagoryPage}
+                      >
+                        Save
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        onClick={navigateToCatagoryPage}
+                      >
+                        Buy
+                      </Button>
+                    </div>
+                  </div>
+                  <div style={{ display: "flex", gap: ".2rem" }}>
+                    <div
+                      style={{
+                        color: "#ff006f",
+                        fontWeight: "700",
+                        fontSize: "0.8rem",
+                      }}
+                    >
+                      Free delivery
+                    </div>
+                    <div>
+                      <CiDeliveryTruck
+                        style={{ fontSize: "1rem", color: "#ff006f" }}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="detailPage_btn">
-              <div>Remove</div>
-              <div>Save</div>
-              <div>Buy</div>
-            </div>
-            <div className="detailPage_">Price details</div>
-            <div className="detailPage_">Place order</div>
           </div>
         </div>
       )}
--- src/Component/ProductPage/Slider.js
+++ src/Component/ProductPage/Slider.js
@@ -1,49 +1,51 @@
 import React from "react";
 import { Carousel } from "antd";
 const contentStyle = {
-
-    height:"220px",
+  height: "220px",
   color: "rgb(255, 255, 255)",
-    lineHeight: "160px",
-    textAlign: "center",
-    background: "rgb(54, 77, 121)",
-    margin: "0px",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "rgb(54, 77, 121)",
+  margin: "0px",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 };
 const Slider = () => {
-  const onChange = (currentSlide) => {
-  };
+  const onChange = (currentSlide) => {};
   return (
     <Carousel afterChange={onChange}>
       <div>
         <h3 style={contentStyle}>
           <img
             alt="example"
-            src={"https://cdn.dummyjson.com/product-images/1/2.jpg"}          />
+            src={"https://cdn.dummyjson.com/product-images/1/2.jpg"}
+          />
         </h3>
       </div>
       <div>
         <h3 style={contentStyle}>
           <img
             alt="example1"
-            src={"https://cdn.dummyjson.com/product-images/8/2.jpg"}          />
+            src={"https://cdn.dummyjson.com/product-images/8/2.jpg"}
+          />
         </h3>
       </div>
       <div>
         <h3 style={contentStyle}>
           <img
             alt="example"
-            src={"https://cdn.dummyjson.com/product-images/13/2.png"}          />
+            src={"https://cdn.dummyjson.com/product-images/13/2.png"}
+          />
         </h3>
       </div>
       <div>
         <h3 style={contentStyle}>
           <img
             alt="example"
-            src={"https://cdn.dummyjson.com/product-images/16/4.jpg"}          />
+            src={"https://cdn.dummyjson.com/product-images/16/4.jpg"}
+          />
         </h3>
       </div>
     </Carousel>
