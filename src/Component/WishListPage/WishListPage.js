4bac1e40470ea6a8f6ae3f71e2c7010b1decaaae
MuskanSrivastava8 <muskansrivastava8756@gmail.com>
2024-05-28 21:16:18 +0530

    WishList Page updated now

--- src/Component/WishListPage/WishListPage.js
+++ src/Component/WishListPage/WishListPage.js
@@ -2,47 +2,18 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ItemCard from "../ProductPage/ItemCard";
 import "./WishListPage.scss";
-import Button from "@mui/material/Button";
-import { Navigate } from "react-router-dom";
-import { IoIosArrowRoundBack } from "react-icons/io";
-import { useDispatch } from "react-redux";
-import { setShowWishListComponent } from "../../Store/store";
-import { useNavigate } from "react-router-dom";
-
 
 function WishListPage() {
   const showWishListResp = useSelector((store) => store.STORE.wishlist);
   const itemData = useSelector((store) => store.STORE.itemData);
-  const showWishListComponentResp = useSelector((store) => store.STORE.showWishListComponent);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   let wishListDetails = showWishListResp.map((i) => {
     return itemData.filter((data) => {
       return data.id === i;
     });
   });
-  const [showHomePage, setshowHomePage] = useState(false);
-  const navigatetoHomePage = () => {
-    dispatch(setShowWishListComponent(true));   
-    // setshowHomePage(true);
-    // if(setshowHomePage){
-    //   navigate("/ProductPage");
-    // }
-    navigate("/ProductPage");
-  };
-
   return (
     <>
-      {/* {!showWishListComponentResp ? <Navigate to="/ProductPage" replace={true} /> : null} */}
-      <div className="wishList_btn_div">
-          <Button variant="contained" size="small" onClick={navigatetoHomePage}>
-            <IoIosArrowRoundBack style={{ fontSize: "2rem" }} />
-          </Button>
-        </div>
       <div className="Wishlist_main_div">
-        
         {wishListDetails &&
           wishListDetails.map((i) => {
             return i.map((item) => {
